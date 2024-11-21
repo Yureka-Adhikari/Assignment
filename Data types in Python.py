e5957eb4cec257d9083af7242298e276a019daae
Yureka Adhikari <adhikariyureka@gmail.com>
2024-11-21 18:44:25 +0545

    first commit

---
+++ Data types in Python.py
@@ -0,0 +1,3 @@
+congratulations=input("Enter Congratulations message in capital letters")
+
+print(f"LowerCase Version: {congratulations.lower()}")
---
+++ Python operators-1.py
@@ -0,0 +1,4 @@
+num=input("Enter the number you'd like the square root of:")
+num=int(num)
+sqrt= num**(1/2)
+print(sqrt) 
