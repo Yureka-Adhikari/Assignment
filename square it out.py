be572d2f56aa7128a0291b2c26a20636278842b5
Yureka Adhikari <adhikariyureka@gmail.com>
2025-01-22 16:10:09 +0545

    second

---
+++ square it out.py
@@ -0,0 +1,26 @@
+def square_of_numbers(start, end):
+    p = start
+    even = []
+    odd = []
+    square = []
+    while p <= end:
+        r = p**2
+        square.append(r)
+        
+        if r % 2 == 0: 
+            even.append(r)
+        else:
+            odd.append(r)
+        p = p+1
+    return even
+s= int(input("Enter the starting number: "))
+e= int(input("Enter the ending number: "))
+
+square_of_numbers(s,e)
+
+
+#print(f"The numbers when squared: {square}")
+
+#print(f"The even numbers when squared: {even}")
+
+#print(f"The odd numbers when squared: {odd}")
